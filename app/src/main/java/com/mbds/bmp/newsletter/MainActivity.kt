9ea1a85591c2efbb2c0c58450fe1315ac5e17429
William POITEVIN <william.poitevin@gmail.com>
2020-11-22 14:59:34 +0100

    add toolbar show aboutUs

--- app/src/main/java/com/mbds/bmp/newsletter/MainActivity.kt
+++ app/src/main/java/com/mbds/bmp/newsletter/MainActivity.kt
@@ -2,27 +2,48 @@ package com.mbds.bmp.newsletter
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.mbds.bmp.newsletter.databinding.ActivityMainBinding
+import com.mbds.bmp.newsletter.fragments.AboutUsFragment
 import com.mbds.bmp.newsletter.fragments.CategoriesFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var menu: Menu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(baseContext))
-
         changeFragment(CategoriesFragment(), false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (menu != null) {
+            this.menu = menu
+            menuInflater.inflate(R.menu.menu, menu)
+            return true
+        }
+
+        return false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.aboutUs) {
+            changeFragment(AboutUsFragment())
+            return true
+        }
+        return false
     }
 
     fun changeFragment(fragment: Fragment, addToBackStack: Boolean = true) {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.fragment_container, fragment)
-            if(addToBackStack)
+            if (addToBackStack)
                 addToBackStack(null)
         }.commit()
     }
---
+++ app/src/main/java/com/mbds/bmp/newsletter/fragments/AboutUsFragment.kt
@@ -0,0 +1,18 @@
+package com.mbds.bmp.newsletter.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mbds.bmp.newsletter.R
+
+class AboutUsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_us, container, false)
+    }
+}
